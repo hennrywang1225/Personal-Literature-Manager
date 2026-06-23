07aa1cba7ca6cb920d5612e675f4852a2609a944
Nantes <hennrywang1225@gmail.com>
2026-06-23 09:49:44 +0800

    feat: wire electron main services

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,7 +1,15 @@
-import { join } from 'node:path'
+import { dirname, join } from 'node:path'
 import { app, BrowserWindow } from 'electron'
+import { openLibraryDatabase } from './database'
+import { createDocumentRepository } from './documentRepository'
+import { createExportService } from './exportService'
+import { createFileStore } from './fileStore'
+import { createImportService } from './importService'
+import { registerIpcHandlers } from './ipcHandlers'
+import { buildLibraryPaths, resolveLibraryRoot } from './libraryPaths'
+import { extractPdfMetadata } from './pdfMetadata'
 
-const createWindow = (): void => {
+const createWindow = (): BrowserWindow => {
   const mainWindow = new BrowserWindow({
     width: 1320,
     height: 860,
@@ -26,10 +34,45 @@ const createWindow = (): void => {
   } else {
     void mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  return mainWindow
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
+  const root = resolveLibraryRoot({
+    isPackaged: app.isPackaged,
+    appDataPath: app.getPath('appData'),
+    exeDir: dirname(app.getPath('exe')),
+  })
+  const paths = buildLibraryPaths(root)
+  const db = await openLibraryDatabase({ databasePath: paths.databasePath })
+  const repo = createDocumentRepository(db)
+  const store = createFileStore({ filesDir: paths.filesDir })
+  const importService = createImportService({ repo, store, extractPdfMetadata })
+  const exportService = createExportService({
+    libraryRoot: root,
+    exportsDir: paths.exportsDir,
+    getSnapshot: repo.getSnapshot,
+  })
+
   createWindow()
+
+  registerIpcHandlers({
+    repo,
+    importService,
+    saveDatabase: () => db.save(),
+    getFileUrl(documentId) {
+      const document = repo.getDocument(documentId)
+      return store.toFileUrl(join(root, document.storedFilePath))
+    },
+    openExternal(documentId) {
+      const document = repo.getDocument(documentId)
+      return store.openExternal(join(root, document.storedFilePath))
+    },
+    exportSelection: exportService.exportSelection,
+    exportCategory: exportService.exportCategory,
+    exportAll: exportService.exportAll,
+  })
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
